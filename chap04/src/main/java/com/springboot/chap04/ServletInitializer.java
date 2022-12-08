9be7b9a70ba57dc33f590a0fb1e338d9c7d24c3c
윤승재 <sjyun011278@gmail.com>
2022-12-08 23:59:26 +0900

    making table

---
+++ chap04/src/main/java/com/springboot/chap04/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.springboot.chap04;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GuestbookApplication.class);
+    }
+
+}

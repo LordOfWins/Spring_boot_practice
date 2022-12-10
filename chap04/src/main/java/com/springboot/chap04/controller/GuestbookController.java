784ea91d91f02baa3209a35ace40b2d621e11bee
윤승재 <sjyun011278@gmail.com>
2022-12-10 22:59:31 +0900

    Paging

--- chap04/src/main/java/com/springboot/chap04/controller/GuestbookController.java
+++ chap04/src/main/java/com/springboot/chap04/controller/GuestbookController.java
@@ -1,17 +1,45 @@
 package com.springboot.chap04.controller;
 
+import com.springboot.chap04.dto.GuestbookDto;
+import com.springboot.chap04.dto.PageRequestDTO;
+import com.springboot.chap04.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
- @GetMapping({"/","/list"})
-    public String list(){
-     log.info("list..........");
-     return "/guestbook/list";
- }
+
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+@GetMapping("/list")
+    public void list(@ModelAttribute("pageRequestDTO")PageRequestDTO pageRequestDTO, Model model){
+    log.info("list........"+pageRequestDTO);
+    model.addAttribute("result", service.getList(pageRequestDTO));
 }
+@GetMapping("/register")
+    public void register(){
+    log.info("register get...");
+}
+@PostMapping("/register")
+    public String registerPost(GuestbookDto dto, RedirectAttributes redirectAttributes){
+    log.info("dto..."+dto);
+    Long gno=service.register(dto);
+    redirectAttributes.addFlashAttribute("msg",gno);
+    return "redirect:/guestbook/list";
+}
+}
+
